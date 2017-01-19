55ff574134e03b01cf5ee7ae9f721045aaf8bac9
admin <admin@test.com>
2017-01-19 12:20:14 +0530

    Home controller changed to add default values for search

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -1,33 +1,75 @@
 (function() {
-    'use strict';
+	'use strict';
 
-    angular
-        .module('bfalegacyApp')
-        .controller('HomeController', HomeController);
+	angular.module('bfalegacyApp').controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+	HomeController.$inject = [ '$rootScope' ,'$http','$scope', 'Principal', '$state','Base64' ];
 
-    function HomeController ($scope, Principal, LoginService, $state) {
-        var vm = this;
+	function HomeController($rootScope,$http,$scope, Principal, $state,Base64) {
+		var vm = this;
+		vm.searchF = {};
+		vm.searchF.origin = 'NYC';
+		vm.searchF.destination = 'SFO';
+		vm.searchF.flightDate = '2017-01-01';
+		vm.flights = {};
 
-        vm.account = null;
-        vm.isAuthenticated = null;
-        vm.login = LoginService.open;
-        vm.register = register;
-        $scope.$on('authenticationSuccess', function() {
-            getAccount();
-        });
+		vm.searchFlight = searchFlight;
+		vm.bookFlight = bookFlight;
+		
+		
+		
+		function bookFlight(origin,destination,flightNumber,flightDate,fare){
+			
+			//Temporarily binding to rootScope
+			//Should be moved to service 
+			$rootScope.flightOrigin = origin;
+			$rootScope.flightDestination = destination;
+			$rootScope.flightNumber = flightNumber;
+			$rootScope.flightDate = flightDate;
+			$rootScope.flightFare = fare;
+			
+			$state.go('book');
+			
+			
+		}
 
-        getAccount();
+		function searchFlight() {
 
-        function getAccount() {
-            Principal.identity().then(function(account) {
-                vm.account = account;
-                vm.isAuthenticated = Principal.isAuthenticated;
-            });
-        }
-        function register () {
-            $state.go('register');
-        }
-    }
+			var data = {
+
+				"origin" : vm.searchF.origin,
+				"destination" : vm.searchF.destination,
+				"flightDate" : vm.searchF.flightDate
+
+			};
+
+			return $http
+					.post(
+							'/api/searchFlights',
+							data,
+							{
+								headers : {
+									'Content-Type' : 'application/json',
+									'Accept' : 'application/json',
+									'Authorization' : 'Basic '
+											+ Base64
+													.encode('legacypssapp'
+															+ ':'
+															+ 'my-secret-token-to-change-in-production')
+								}
+							}).success(authSucess);
+
+			function authSucess(response) {
+				var expiredAt = new Date();
+				expiredAt.setSeconds(expiredAt.getSeconds()
+						+ response.expires_in);
+				response.expires_at = expiredAt.getTime();
+				vm.flights = response;
+				vm.hasFlights = true;
+				return response;
+			}
+
+		}
+
+	}
 })();
